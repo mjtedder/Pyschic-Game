14c59bb70cf999b29b9b8141977c91ea9094c373
unknown <mjtedder@gmail.com>
2018-03-31 00:20:09 -0400

    game logic working. updated basic styling.  update more later

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,73 @@
-var d = new Date();
-document.body.innerHTML = ("<h1>Todays date is: " + d + "</h1>");
+// Tester Code
+//var d = new Date();
+//document.body.innerHTML = ("<h1>Todays date is: " + d + "</h1>");
 
-// PSEUDO CODE FOR PSYCHIC GAME
-      //Create Array of Letters (var options = ["A", . . . ]) for user to choose from;
-      //var computerChoice executes the function to choose a random letter;
-          //-Computer chooses a random letter
-          var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-          //create wins variable (var wins = 0);
-          var wins = 0;
-          //create losses variable (var losses = 0);
-          var losses = 0;
-          //Create guessesLeft variable (var guesses = 9);
-          var guessesLeft = 9;
-
-          var guessesSoFar [];
-
-          var userGuess = null;
+// Define alphabet letters that computer can pick from
+var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Set the initial global variables
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+// guessesSoFar is an array that will hold all the user's guesses in each round
+var guessesSoFar = [];
+// userGuess is what the user picks by pressing a key
+var userGuess = null;
+// Have computer pick a letter and store it in letterToBeGuessed
+var letterToBeGuessed = computerOptions[Math.floor(Math.random) * computerOptions.length];
+// start listening for events
+document.onkeyup = function(event) {
 
 
-      //Capture userGuess onkeyup guess  (make sure to use .toLowerCase);
 
-          document.onkeyup = function(event) {
 
-            var userGuess = event.key.toLowerCase();
+	// When user presses a key, it records it and saves to userGuess
+var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-            var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-      //record letters (var guessesSoFar (event.onkey) chosen by UserGuess;
 
-            var guessesSoFar = event.onkeyup
+	// If the userGuess is not in the index of guessesSoFar
+  // AND userGuess is a letter of the alphabet,
+  //THEN add the userGuess to guessesSoFar array.
+
+  if (guessesSoFar.indexOf(userGuess) < 0 && computerOptions.indexOf(userGuess) >= 0) {
+    guessesSoFar[guessesSoFar.length] = userGuess;
+
+  // if it is a new letter then decrease remaining guesses by 1
+    guessesLeft--;
+  }
+
+	// if letterToBeGuessed is same as userGuess then record it as a win
+	// and then reset guessesLeft to 9, and empty the guessesSoFar array
+	// also have the computer make a new random pick
+  if (letterToBeGuessed == userGuess){
+    wins++;
+    console.log("You win!");
+    guessesLeft = 9;
+    guessesSoFar = [];
+    letterToBeGuessed = computerOptions[Math.floor(Math.random) * computerOptions.length];
+  }
+
+	// if guessesLeft gets to 0 then record it as a loss
+	// and then reset guessesLeft to 9, and empty the guessesSoFar array
+	// also have the computer make a new random pick
+  if (guessesLeft == 0) {
+    losses++;
+    guessesLeft = 9;
+    guessesSoFar = [];
+    lettertoBeGuessed = computerOptions[Math.floor(Math.random) * computerOptions.length];
+  }
+
+	// Displaying progress to HTML
+
+  var html =
+  "<h1>The Pyschic Game</h1><br>" +
+  "<h3>Guess what letter I'm thinking of</h3><br>" +
+  "<h3>Wins: " + wins + "</h3><br>" +
+  "<h3>Losses: " + losses + "</h3><br>" +
+  "<h3>Guesses Left: " + guessesLeft + "</h3><br>" +
+  "<h3>Your Guesses so far: " + guessesSoFar + "</h3><br>";
+
+
+	// place html into the game ID
+  document.querySelector("#game").innerHTML = html;
 }
-
-
-
-
-
-
-      //guessesLeft-- (i-1) to subtract a guess on a wrong answer;
-
-      //Compare userGuess to Computerchoice  (if, else/if, else)
-          //if userGuess = computerChoice, wins++
-          //if userGuess doesn't equal computer choice, guessesLeft--
-          //if guessesLeft reaches less than 1 and doesn't match computerChoice add +1 to losses (losses++)
-
-          //when wins++ or losses++
-          //reset guessesLeft back to 9
-          //reset guessSoFar back to emptyArray
-          //execute computerChoice again on the reset  -->
